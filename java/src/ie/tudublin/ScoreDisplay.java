e3699328d5a13456f2ee8d96dca51f37f670fc1c
Wedaad <wedaadh2@gmail.com>
2021-04-21 16:15:09 +0100

    Declared array list of type Note called notes

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -11,7 +11,8 @@ public class ScoreDisplay extends PApplet
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
-	//ArrayList
+	//array list to holds the instances of the note class
+	ArrayList<Note> notes = new ArrayList<Note>(); 
 	
 	public void settings()
 	{
